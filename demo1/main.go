a40e4e29476021e2d76b186fdd671e5c156b2c58
laojiaqing <810365923@qq.com>
2019-08-24 17:51:00 +0800

    123

---
+++ demo1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	var (
+		cmd *exec.Cmd
+		err error
+	)
+
+	//cmd = exec.Command("/bin/bash", "-c", "echo 1;echo 2;")
+	cmd = exec.Command("C:\\cygwin64\\bin\\bash.exe", "-c", "echo 1")
+
+	err = cmd.Run()
+
+	fmt.Println(err)
+}
--- demo2/main.go
+++ demo2/main.go
@@ -1,1 +1,23 @@
-package demo2
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	var (
+		cmd *exec.Cmd
+		output []byte
+		err error
+	)
+
+	cmd = exec.Command("C:\\cygwin64\\bin\\bash.exe", "-c", "sleep 5; ls -l")
+
+	if output, err = cmd.CombinedOutput(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(output)
+}
